90d6a60785b9402d8e411e636fc64ffa010c5af1
Edwin Portillo <weaselssh@gmail.com>
2025-08-22 08:26:59 -0600

    rentitem

--- ExamenP2_Parcial1/src/examenp2_parcial1/ExamenP2_Parcial1.java
+++ ExamenP2_Parcial1/src/examenp2_parcial1/ExamenP2_Parcial1.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package examenp2_parcial1;
 
-/**
- *
- * @author edwin
- */
 public class ExamenP2_Parcial1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
