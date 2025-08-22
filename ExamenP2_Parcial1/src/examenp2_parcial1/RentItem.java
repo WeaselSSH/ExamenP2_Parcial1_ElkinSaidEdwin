5ea56a02d7c8f86c67b4d8df0813e98c4d78cdc4
Edwin Portillo <weaselssh@gmail.com>
2025-08-22 08:23:15 -0600

    rentItem

---
+++ ExamenP2_Parcial1/src/examenp2_parcial1/RentItem.java
@@ -0,0 +1,13 @@
+package examenp2_parcial1;
+
+import javax.swing.ImageIcon;
+
+public abstract class RentItem {
+
+    int codigoItem;
+    String nombreItem;
+    double precioRenta;
+    ImageIcon item;
+    int cantidadCopias  = 0;
+}
+
